4c79f6096695a66fddca3ef6cb5389b11ddfe1b2
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-01-27 15:21:26 +0900

    見た目の作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,40 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Heading, Box, Button, Text } from '@chakra-ui/react';
 
-const inter = Inter({ subsets: ["latin"] });
+import { Heading, Box, Button, Text } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react'
+import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+
+
 
 export default function Home() {
   return (
-    <Box width="200px" background="gray">
-      <Text color="blue" fontWeight="bold">
-        テキスト
-      </Text>
-      <Heading>見出し</Heading>
-      <Button>ボタン</Button>
-    </Box>
+    <ChakraProvider>
+      <Box w="fit-content" m="40px auto">
+        <Heading marginBottom="12px">日付</Heading>
+        <Box w="520px" bg="orange.50" p="40px 50px">
+          <Text marginBottom={8} fontWeight="bold">
+            未完了
+          </Text>
+          <Stack marginBottom={16} spacing={6}>
+            <Checkbox spacing={5}>タスク</Checkbox>
+            <Checkbox spacing={5}>タスク</Checkbox>
+            <Checkbox spacing={5}>タスク</Checkbox>
+          </Stack>
+          <Text marginBottom={8} fontWeight="bold">
+            完了
+          </Text>
+          <Stack spacing={6}>
+            <Checkbox spacing={5} defaultChecked>タスク</Checkbox>
+            <Checkbox spacing={5} defaultChecked>タスク</Checkbox>
+            <Checkbox spacing={5} defaultChecked>タスク</Checkbox>
+            <Checkbox spacing={5} defaultChecked>タスク</Checkbox>
+          </Stack>
+
+        </Box>
+      </Box>
+    </ChakraProvider>
   );
 }
